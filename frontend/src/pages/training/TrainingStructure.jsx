7135c49411d0fb3586db9a26b66ed88d2dc88e1f
Herold7 <makpier@gmail.com>
2024-06-03 16:36:44 +0200

    Mise à jour de la structure des données pour chaque page des formations

--- frontend/src/pages/training/TrainingStructure.jsx
+++ frontend/src/pages/training/TrainingStructure.jsx
@@ -1,42 +1,43 @@
 import React from 'react';
-import { Section, ColumnsSection, Column, ImageContainer, Heading, Paragraph, List } from './styledComponents';
+import { Container, Columns, Column, Image, Heading, SubHeading, Paragraph, List, ListItem, ButtonContainer, Button } from './styledComponents';
 
 const TrainingStructure = ({ data }) => {
+    const agent = useAgent();
     return (
         <Container>
-          <Columns>
-            <Column>
-              <figure>
-                <Image src={agent.image} alt={agent.imageAlt} />
-              </figure>
-            </Column>
-            <Column>
-              <Heading>{agent.title}</Heading>
-              <Paragraph>{agent.description}</Paragraph>
-            </Column>
-          </Columns>
-          {agent.sections.map((section, index) => (
-            <Column key={index} bgColor={section.bgColor} color={section.color}>
-              <SubHeading>
-                {section.heading}
-                <img src={section.icon} alt={section.iconAlt} />
-              </SubHeading>
-              <Paragraph>{section.description}</Paragraph>
-              <List>
-                {section.items.map((item, idx) => (
-                  <ListItem key={idx}>{item}</ListItem>
-                ))}
-              </List>
-            </Column>
-          ))}
-          <ButtonContainer>
-            <Button href={agent.moreInfoLink} target="_blank" rel="noopener noreferrer">
-              En savoir plus
-            </Button>
-            <Button href={agent.signupLink} outline>
-              Je m'inscris
-            </Button>
-          </ButtonContainer>
+            <Columns>
+                <Column>
+                    <figure>
+                        <Image src={agent.img} alt={agent.imgAlt} />
+                    </figure>
+                </Column>
+                <Column>
+                    <Heading>{agent.title}</Heading>
+                    <Paragraph>{agent.description}</Paragraph>
+                </Column>
+            </Columns>
+            {agent.sections.map((section, index) => (
+                <Column key={index} bgColor={section.bgColor} color={section.color}>
+                    <SubHeading>
+                        {section.heading}
+                        <img src={section.icon} alt={section.iconAlt} />
+                    </SubHeading>
+                    <Paragraph>{section.description}</Paragraph>
+                    <List>
+                        {section.items.map((item, idx) => (
+                            <ListItem key={idx}>{item}</ListItem>
+                        ))}
+                    </List>
+                </Column>
+            ))}
+            <ButtonContainer>
+                <Button href={agent.moreInfoLink} target="_blank" rel="noopener noreferrer">
+                    En savoir plus
+                </Button>
+                <Button href={agent.signupLink} outline>
+                    Je m'inscris
+                </Button>
+            </ButtonContainer>
         </Container>
     );
 };
