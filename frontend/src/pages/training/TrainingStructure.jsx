289d2a026d31a5259928d9140507d3296adc7132
Herold7 <makpier@gmail.com>
2024-06-09 18:48:27 +0200

    Ajout de l'import useAgent

--- frontend/src/pages/training/TrainingStructure.jsx
+++ frontend/src/pages/training/TrainingStructure.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAgent } from './TrainingContext';
 import { Container, Columns, Column, Image, Heading, SubHeading, Paragraph, List, ListItem, ButtonContainer, Button } from './styledComponents';
 
 const TrainingStructure = ({ data }) => {
