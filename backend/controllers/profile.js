fc8256ce92dd1f313458316eb4836aebe91e861b
Herold7 <makpier@gmail.com>
2024-06-12 05:56:28 +0200

    Mise à jour des controllers pour l'envoi en base de données d'un nouveau client et d'un nouveau rendez-vous

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -1,19 +1,17 @@
 const Client = require('../models/client');
+const sendRegistrationEmail = require('../services/emailServices');
 const fs = require('fs');
 
 //Middelwares CRUD
 //Modification, suppression et lecture accessible uniquement aux users authentifiés
 exports.createClient = (req, res, next) => {
-    const clientObject = JSON.parse(req.body.client);
-    delete clientObject._id;
-    delete clientObject._userId;
     const client = new Client({
-        ...clientObject,
+        ...req.body,
         cv: `${req.protocol}://${req.get('host')}/files/${req.file.filename}`//Génération d'une URL complète lors du téléversement du cv
     });
 
     client.save()
-        .then(() => { res.status(201).json({ message: 'Client enregistré !' }) })
+        .then(() => { sendRegistrationEmail(client); res.status(201).json({ message: 'Client enregistré !' }) })
         .catch(error => { res.status(400).json({ error }) })
 };
 
@@ -38,37 +36,54 @@ exports.getOneClient = (req, res, next) => {
 };
 
 exports.modifyClient = (req, res, next) => {
+    // Vérifie que l'utilisateur est authentifié
+    if (!req.auth || !req.auth.userId) {
+        return res.status(401).json({ message: 'Non autorisé' });
+    }
     const clientObject = req.file ? {
         ...JSON.parse(req.body.client),
-        cv: `${req.protocol}://${req.get('host')}/files/${req.file.filename}`
     } : { ...req.body };
 
     delete clientObject._userId;
+
     Client.findOne({ _id: req.params.id })
-        .then((client) => {
-            if (client.userId != req.auth.userId) {//Vérifie qu'un user est bien authentifié pour la modification du profil d'un client
-                res.status(401).json({ message: 'Non autorisé' });
-            } else {
-                Client.updateOne({ _id: req.params.id }, { ...clientObject, _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Client modifié!' }))
-                    .catch(error => res.status(401).json({ error }));
+        .then(client => {
+            if (!client || client.userId !== req.auth.userId) {
+                return res.status(403).json({ message: 'Non autorisé' });
             }
+            // Suppression du fichier précédent si un nouveau fichier est téléchargé
+            if (req.file) {
+                const oldFilename = client.cv.split('/files/')[1];//Récupération du nom du fichier
+                fs.unlink(`files/${oldFilename}`, (err) => {
+                    if (err) console.error('Erreur lors de la suppression de l\'ancien fichier :', err);
+                });
+            }
+            //Mise à jour du rendez-vous
+            client.updateOne({ _id: req.params.id }, { ...clientObject, _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Rendez-vous modifié!' }))
+                .catch(error => res.status(400).json({ error }));
         })
-        .catch((error) => {
-            res.status(400).json({ error });
+        .catch(error => {
+            res.status(500).json({ error });
         });
 };
 
-exports.deleteClient = (req, res, next) => {
+exports.deleteclient = (req, res, next) => {
+    // Vérifie que l'utilisateur est authentifié
+    if (!req.auth || !req.auth.userId) {
+        return res.status(401).json({ message: 'Non autorisé' });
+    }
     Client.findOne({ _id: req.params.id })
         .then(client => {
-            if (client.userId != req.auth.userId) {
-                res.status(401).json({ message: 'Non autorisé' });
-            } else {
-                    Client.deleteOne({ _id: req.params.id })
-                        .then(() => { res.status(200).json({ message: 'Client supprimé !' }) })
-                        .catch(error => res.status(401).json({ error }));
-                };
+            if (!client || client.userId !== req.auth.userId) {
+                return res.status(403).json({ message: 'Non autorisé' });
+            }
+            const filename = client.cv.split('/files/')[1];
+            fs.unlink(`files/${filename}`, () => {//Suppression du fichier du dossier "files"
+                Client.deleteOne({ _id: req.params.id })
+                    .then(() => { res.status(200).json({ message: 'Rendez-vous supprimé !' }) })
+                    .catch(error => res.status(500).json({ error }));
+            });
         })
         .catch(error => {
             res.status(500).json({ error });
--- backend/controllers/registration.js
+++ backend/controllers/registration.js
@@ -1,18 +1,15 @@
 const Interview = require('../models/interview');
-const fs = require('fs');
+const sendRegistrationEmail = require('../services/emailServices');
+
 
 //Middelwares CRUD
 //Modification, suppression et lecture accessible uniquement aux users authentifiés
 exports.createInterview = (req, res, next) => {
-    const interviewObject = JSON.parse(req.body.interview);
-    delete interviewObject._id;
-    delete interviewObject._userId;
     const interview = new Interview({
-        ...interviewObject,
-        cv: `${req.protocol}://${req.get('host')}/files/${req.file.filename}`//Génération d'une URL complète lors du téléversement du cv client
+        ...req.body,
     });
     interview.save()
-        .then(() => { res.status(201).json({ message: 'Rendez-vous enregistré !' }) })
+        .then(() => { sendRegistrationEmail(interview); res.status(201).json({ message: 'Interview enregistré !' }) })
         .catch(error => { res.status(400).json({ error }) })
 };
 
@@ -43,22 +40,13 @@ exports.modifyInterview = (req, res, next) => {
     }
     const interviewObject = req.file ? {
         ...JSON.parse(req.body.interview),
-        cv: `${req.protocol}://${req.get('host')}/files/${req.file.filename}`
     } : { ...req.body };
 
     delete interviewObject._userId;
-
     Interview.findOne({ _id: req.params.id })
         .then(interview => {
             if (!interview || interview.userId !== req.auth.userId) {
                 return res.status(403).json({ message: 'Non autorisé' });
-            }
-            // Suppression du fichier précédent si un nouveau fichier est téléchargé
-            if (req.file) {
-                const oldFilename = interview.cv.split('/files/')[1];//Récupération du nom du fichier
-                fs.unlink(`files/${oldFilename}`, (err) => {
-                    if (err) console.error('Erreur lors de la suppression de l\'ancien fichier :', err);
-                });
             }
             //Mise à jour du rendez-vous
             Interview.updateOne({ _id: req.params.id }, { ...interviewObject, _id: req.params.id })
@@ -80,13 +68,10 @@ exports.deleteInterview = (req, res, next) => {
             if (!interview || interview.userId !== req.auth.userId) {
                 return res.status(403).json({ message: 'Non autorisé' });
             }
-            const filename = interview.cv.split('/files/')[1];
-            fs.unlink(`files/${filename}`, () => {//Suppression du fichier du dossier "files"
                 Interview.deleteOne({ _id: req.params.id })
                     .then(() => { res.status(200).json({ message: 'Rendez-vous supprimé !' }) })
                     .catch(error => res.status(500).json({ error }));
-            });
-        })
+            })
         .catch(error => {
             res.status(500).json({ error });
         });
