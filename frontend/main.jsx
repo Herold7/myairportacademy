49df2f11d85d743300b648b3ac7a5913897dd417
Herold7 <makpier@gmail.com>
2024-06-13 00:31:44 +0200

    Mise à jour des routers et chemins d'accès aux pages

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './src/App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/styles.css';
+import App from './src/pages/home/App.jsx';
+// import Testimonials from './src/pages/testimonial/testimonials.jsx';
+import Training from './src/pages/training/Trainings.jsx';
+import Registration from './src/pages/registration/Course.jsx';
+import Funding from './src/pages/funding/Fundings.jsx';
+// import Contact from './src/pages/contact/Contacts.jsx'
+// import Blog from './src/pages/blog/Blog.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+// Router & routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  // {
+  //   path: '/temoignages',
+  //   element: <Testimonials />,
+  // },
+  {
+    path: '/formations',
+    element: <Training />,
+  },
+  {
+    path: '/inscription',
+    element: <Registration />,
+  },
+  {
+    path: '/financement',
+    element: <Funding />,
+  },
+  // {
+  //   path: '/contact-faq',
+  //   element: <Contact />,
+  //   },
+  // {
+  //   path: '/blog',
+  //   element: <Blog />,
+  //   },
+
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
