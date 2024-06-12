d6ba2ef5ede1d23d82a2950e8e5624a3b9756f40
Herold7 <makpier@gmail.com>
2024-06-12 02:47:45 +0200

    Mise à jour du style avec bootstrap

--- frontend/src/pages/registration/Course.jsx
+++ frontend/src/pages/registration/Course.jsx
@@ -8,16 +8,14 @@ import './Course.css'; // Assurez-vous d'importer le fichier CSS correspondant
 const Course = () => {
     return (
         <section className="full-width-section">
-            <div className="group-section" style={{ marginTop: 'var(--wp--preset--spacing--60)', marginBottom: 'var(--wp--preset--spacing--60)', paddingTop: 0, paddingBottom: 0 }}>
-                <div className="content-section" style={{ paddingRight: 'var(--wp--preset--spacing--50)', paddingLeft: 'var(--wp--preset--spacing--50)' }}>
-                    <h2 className="heading has-text-align-center has-kenta-font-large-font-size" style={{ marginTop: 0, marginBottom: 0 }}>
-                        Déroulé du processus de formation
-                    </h2>
-                    <p className="paragraph has-text-align-center" style={{ marginTop: '12px', marginBottom: '48px' }}>
-                        De la définition de vos besoins en terme de formation, jusqu'à l'obtention de votre certification voire au-delà, nous vous accompagnons afin que votre parcours soit une réussite.
+            <div className="container my-5">
+                <div className="text-center mb-5">
+                    <h2 className="display-4">Déroulé du processus de formation</h2>
+                    <p className="lead">
+                        De la définition de vos besoins en termes de formation, jusqu'à l'obtention de votre certification voire au-delà, nous vous accompagnons afin que votre parcours soit une réussite.
                     </p>
                 </div>
-                <div className="columns-section alignwide" style={{ paddingRight: 'var(--wp--preset--spacing--50)', paddingLeft: 'var(--wp--preset--spacing--50)' }}>
+                <div className="row">
                     {[
                         {
                             imgSrc: assessment,
@@ -40,11 +38,11 @@ const Course = () => {
                             title: "Entretien d'embauche"
                         }
                     ].map((item, index) => (
-                        <div className="column" key={index}>
-                            <div className="cover" style={{ borderRadius: '16px', minHeight: '430px', padding: 'var(--wp--preset--spacing--50)' }}>
-                                <img className="cover-image" src={item.imgSrc} alt={item.altText} style={{ objectFit: 'cover' }} />
-                                <div className="cover-inner-container">
-                                    <p className="cover-title" style={{ fontStyle: 'normal', fontWeight: 'bold', color: 'white' }}>{item.title}</p>
+                        <div className="col-md-6 col-lg-3 mb-4" key={index}>
+                            <div className="card h-100" style={{ border:'none', borderRadius:'16px', overflow:'hidden'}}>
+                                <img className="card-img-top" src={item.imgSrc} alt={item.altText} style={{ objectFit: 'cover', borderTopLeftRadius: '16px', borderTopRightRadius: '16px' }} />
+                                <div className="card-body d-flex align-items-end p-" style={{ backgroundColor: '#000', borderBottomLeftRadius: '16px', borderBottomRightRadius: '16px', padding: '1rem' }}>
+                                    <p className="card-title text-white mb-0" style={{ fontStyle: 'normal', fontWeight: 'bold' }}>{item.title}</p>
                                 </div>
                             </div>
                         </div>
